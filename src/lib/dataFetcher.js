a32ad6670029c9339d7e704494d64728182b1d36
Kristofer Masson <krm21@hi.is>
2016-11-02 17:39:42 +0000

    fb API call works

--- src/lib/dataFetcher.js
+++ src/lib/dataFetcher.js
@@ -1,20 +1,19 @@
 
-const EventSearch = require('facebook-events-by-location-core')
+const EventSearch = require('facebook-events-by-location-core');
 
-exports.getStuff = function(req, res, next) {
-
-  let es = new EventSearch({
-      "accessToken": "fb24916f592c9dd9dcb26c998eb0a28a",
-      "lat": 40.710803,
-      "lng": -73.964040
+exports.getStuff = function (req, res, next) {
+  const es = new EventSearch({
+    accessToken: '1729664434025571|fb24916f592c9dd9dcb26c998eb0a28a',
+    lat: 40.710803,
+    lng: -73.964040,
   });
 
-  es.search().then(function (events) {
-      console.log(JSON.stringify(events));
-  }).catch(function (error) {
-      console.error(JSON.stringify(error));
+  es.search().then((events) => {
+    console.log(JSON.stringify(events));
+  }).catch((error) => {
+    console.error(JSON.stringify(error));
   });
-  req.value = "Virkar thetta ?"
+  req.value = 'Virkar thetta ?';
 
   next();
-}
+};
