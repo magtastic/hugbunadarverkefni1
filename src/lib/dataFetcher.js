9cc9ce065588de94960127326d35b36779cf1017
sveinnvi <sveinnvi@gmail.com>
2016-11-02 17:53:30 +0000

    gitignore, eslint

--- src/lib/dataFetcher.js
+++ src/lib/dataFetcher.js
@@ -3,7 +3,6 @@ const EventSearch = require('facebook-events-by-location-core');
 
 exports.getStuff = function (req, res, next) {
   const es = new EventSearch({
-    accessToken: '1729664434025571|fb24916f592c9dd9dcb26c998eb0a28a',
     lat: 40.710803,
     lng: -73.964040,
   });
