5d5018fca82655f2e44c750f7fdbe123b400423f
Magtastic <Magnusol92@gmail.com>
2016-11-02 18:14:32 +0000

    ip tala kominn

---
+++ src/public/main.js
@@ -0,0 +1,13 @@
+var requst = new XMLHttpRequest();
+requst.open('GET', "http://jsonip.com/?callback=", true);
+requst.onreadystatechange = processRequest;
+requst.send();
+
+function processRequest(e){
+    if (requst.readyState == 4 && requst.status == 200) {
+        var response = JSON.parse(requst.responseText);
+        console.log(response);
+        var myIp = response.ip;
+    }
+}
+
