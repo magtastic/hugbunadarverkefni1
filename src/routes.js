933dabd32fe98c1af4d5f3b117e04da81b5395d8
Kristofer Masson <krm21@hi.is>
2016-11-02 15:50:04 +0000

    Setup Express and Pug

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const title = 'Heimasíðan okkar';
+  const data = { title };
+  res.render('index', data);
+});
+
+router.post('/', (req, res, next) => {
+  const title = 'Niðurstöður margföldunar';
+  const data = { title};
+  res.render('index', data);
+});
+
+module.exports = router;
