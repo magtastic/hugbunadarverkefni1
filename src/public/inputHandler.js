321bcc08526f05fe7e8764f8183544256c76fd27
Magtastic <Magnusol92@gmail.com>
2016-11-06 19:59:54 +0000

    search thing

---
+++ src/public/inputHandler.js
@@ -0,0 +1,12 @@
+var searchBar = $("#searchBar");
+
+function searchLocation(){
+    console.log(searchBar.val());
+    console.log("here");
+}
+
+searchBar.keyup(function(event){
+    if(event.keyCode == 13){
+        searchBar.click();
+    }
+});
