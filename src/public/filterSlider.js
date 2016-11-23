adf9be8dcc3580769d730493f3de3e06e8e6fa1b
Magtastic <Magnusol92@gmail.com>
2016-11-23 19:04:00 +0000

    filter dot

--- src/public/filterSlider.js
+++ src/public/filterSlider.js
@@ -6,3 +6,29 @@ jQuery(function($)
 
     })
 });
+
+ $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+          console.log("event: " + event);
+          console.log("ui: " + ui);
+      }
+    });
+  } );
+  
+ $( function() {
+    $( "#slider-range_1" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+          console.log("event: " + event);
+          console.log("ui: " + ui);
+      }
+    });
+  } );
