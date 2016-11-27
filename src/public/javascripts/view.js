ecf499fcd75d4c98fcb6ce6edb13124ee99bfcee
Magtastic <Magnusol92@gmail.com>
2016-11-27 22:03:30 +0000

    buinn að laga þetta dæmi með sliders og filters en buggið er ennþá, þ.e. maður þarf að fikta í sliderunum til að sjá events

--- src/public/javascripts/view.js
+++ src/public/javascripts/view.js
@@ -63,77 +63,6 @@ function initEventCards() {
   });
 }
 
-//open filters
-jQuery(function($)
-{
-    $(".hamburger").click(function()
-    {
-        $(".navigation").toggleClass("open");
-
-    });
-});
-
-//init date slider
- $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 7,
-      values: [ 0, 1],
-      slide: function( event, ui ) {
-          var handles = $("#slider-range > span");
-          handles[0].innerHTML = ui.values[0];
-          handles[1].innerHTML = ui.values[1];
-          var startDate = new Date();
-          var endDate = new Date();
-          startDate.setDate(startDate.getDate()+ui.values[0]);
-          endDate.setDate(endDate.getDate()+ui.values[1]);
-
-          updateFilter("DATE", startDate, endDate);
-          hideEvents();
-          filterEventsByTime();
-          filterEventsByAttenders();
-          showEvents();
-      }
-    });
-    var handles = $("#slider-range > span");
-    handles[0].innerHTML = 0;
-    handles[1].innerHTML = 1;
-  });
-
-//init attending slider
- $( function() {
-    $( "#slider-range_1" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ filters.minAttenders, 500 ],
-      slide: function( event, ui ) {
-          var handles = $("#slider-range_1 > span");
-          handles[0].innerHTML = ui.values[0];
-          handles[1].innerHTML = ui.values[1];
-          updateFilter("ATTENDEES", ui.values[0], ui.values[1]);
-          hideEvents();
-          filterEventsByTime();
-          filterEventsByAttenders();
-          showEvents();
-      }
-    });
-    var handles = $("#slider-range_1 > span");
-    handles[0].innerHTML = 0;
-    handles[1].innerHTML = 500;
-  } );
-
-  function updateFilter(type, min, max) {
-    if(type === "ATTENDEES") {
-      filters.minAttenders = min;
-      filters.maxAttenders = max;
-    }else if(type === "DATE"){
-      filters.startTime = min;
-      filters.endTime = max;
-    }
-  }
-
 
 //init the google geocoder
 function initGoogleAPI(){
@@ -323,3 +252,76 @@ function loadFiltersInLocalStorage(){
      filters.maxAttenders = lf.maxAttenders;
    }
  }
+
+//open filters
+jQuery(function($)
+{
+    $(".hamburger").click(function()
+    {
+        $(".navigation").toggleClass("open");
+
+    });
+});
+
+//init date slider
+ $( function() {
+    console.log(filters.startTime.getDay());
+    console.log(filters.endTime.getDay());
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 7,
+      values: [ filters.startTime.getDay(),filters.endTime.getDay()],
+      slide: function( event, ui ) {
+          var handles = $("#slider-range > span");
+          handles[0].innerHTML = ui.values[0];
+          handles[1].innerHTML = ui.values[1];
+          var startDate = new Date();
+          var endDate = new Date();
+          startDate.setDate(startDate.getDate()+ui.values[0]);
+          endDate.setDate(endDate.getDate()+ui.values[1]);
+
+          updateFilter("DATE", startDate, endDate);
+          hideEvents();
+          filterEventsByTime();
+          filterEventsByAttenders();
+          showEvents();
+      }
+    });
+    var handles = $("#slider-range > span");
+    handles[0].innerHTML = 0;
+    handles[1].innerHTML = 1;
+  });
+
+//init attending slider
+ $( function() {
+    $( "#slider-range_1" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ filters.minAttenders, 500 ],
+      slide: function( event, ui ) {
+          var handles = $("#slider-range_1 > span");
+          handles[0].innerHTML = ui.values[0];
+          handles[1].innerHTML = ui.values[1];
+          updateFilter("ATTENDEES", ui.values[0], ui.values[1]);
+          hideEvents();
+          filterEventsByTime();
+          filterEventsByAttenders();
+          showEvents();
+      }
+    });
+    var handles = $("#slider-range_1 > span");
+    handles[0].innerHTML = 0;
+    handles[1].innerHTML = 500;
+  } );
+
+  function updateFilter(type, min, max) {
+    if(type === "ATTENDEES") {
+      filters.minAttenders = min;
+      filters.maxAttenders = max;
+    }else if(type === "DATE"){
+      filters.startTime = min;
+      filters.endTime = max;
+    }
+  };
